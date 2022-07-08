7bee40c50c0fbfac1c7454d6f460fe44a29c641f
Louis-sf <louis3699>
2022-07-08 13:55:00 -0700

    some auth set up with reddit api/produced unstructured json to "reddit1" topic

---
+++ reddit_api_sandbox.py
@@ -0,0 +1,36 @@
+import json
+import requests
+import requests.auth
+import ccloud_lib
+from confluent_kafka import Producer
+
+client_key = "34p9yVEI4vWN7YgOLv_phA"
+client_secret = "MmVidcg43Lab7ckKZK6kvt8IGe_7Dw"
+auth = requests.auth.HTTPBasicAuth(client_key,client_secret)
+post_data = {"grant_type": "password",
+             "username": "reddit_user3699",
+             "password": "Fe@5$yfcMGqRxP5"}
+print(auth)
+headers = {"User-Agent": "MyAPI/0.1 by reddit_user3699"}
+res = requests.post("https://www.reddit.com/api/v1/access_token",
+                         auth=auth, data=post_data, headers=headers)
+print(res.json())
+token =  res.json()['access_token']
+headers = {**headers, **{'Authorization': f'bearer {token}'}}
+
+# info = requests.get("https://oauth.reddit.com/api/v1/me", headers = headers)
+# print(info.json())
+config_file = "python.config"
+topic = "reddit1"
+conf = ccloud_lib.read_ccloud_config(config_file)
+producer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
+redditproducer = Producer(producer_conf)
+py_subreddit_info = requests.get("https://oauth.reddit.com/r/python/new", headers = headers)
+print(py_subreddit_info.json()['data']['children'][0]['data'].keys())
+# for post in py_subreddit_info.json()['data']['children']:
+#     print(post['data']['title'])
+#     redditproducer.produce(topic, value=post)
+# print(py_subreddit_info.json()['data'])
+
+
+
