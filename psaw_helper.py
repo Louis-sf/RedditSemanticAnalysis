ca0b1d9767f6083b52a0de44ec1af4a13159c282
Louis-sf <louis3699>
2022-07-21 10:50:18 -0700

    changed helper so that user can extract more than 100 records of data everytime

--- psaw_helper.py
+++ psaw_helper.py
@@ -5,14 +5,23 @@ import requests
 
 
 def get_pushshift_data(after, before, sub):
-    #Build URL
-    url = 'https://api.pushshift.io/reddit/search/submission/?sort = desc'+'&limit=1000&after='+str(after)+'&before='+str(before)+'&subreddit='+str(sub)
-    #Print URL to show user
-    #print(url)
-    #Request URL
-    r = requests.get(url)
-    #Load JSON data from webpage into data variable
-    data = json.loads(r.text)
-    #return the data element which contains all the submissions data
-    return data['data']
+    result = []
+    start = after
+    while True:
+        #Build URL
+        url = 'https://api.pushshift.io/reddit/search/submission/?sort = desc'+'&limit=1000&after='+str(start)+'&before='+str(before)+'&subreddit='+str(sub)
+        r = requests.get(url)
+        #Load JSON data from webpage into data variable
+        initial_data = json.loads(r.text)
+        # if initial_data.length['data'] == 0:
+        #     break
+        init_len = len(result)
+        result += initial_data['data']
+        updated_len = len(result)
+        if updated_len == init_len:
+            break
+        start = (result[len(result) - 1])['created_utc']
+    return result
 
+
+print(len(get_pushshift_data(1626825952,1626912352,"nba")))
